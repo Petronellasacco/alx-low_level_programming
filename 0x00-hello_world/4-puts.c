833c9807f311dde35cf77628ad4fbdb7689ffa96
Petronellasacco <petronellatebatso705@gmail.com>
2023-07-14 02:15:12 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that prints a new line with puts function
+ *
  * Return: 0 (success)
  */
 int main(void)
 {
-	puts("\Programming is like building a multilingual puzzle")
+	puts("\Programming is like building a multilingual puzzle\n");
 	return (0);
 }
