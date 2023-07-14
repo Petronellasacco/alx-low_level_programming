7aa2d5eb3421de36c512f770056d86900cf1db46
Petronellasacco <petronellatebatso705@gmail.com>
2023-07-14 02:34:28 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
-*main – Entry point
+* main – Entry point
 *
-*Return: Always 0 (success)
+* Return: Always 0 (success)
 */
 int main(void)
 {
-	Puts(“with proper grammar, but the outcome is a piece of art,\n”);
-	Return (0);
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
