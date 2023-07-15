54d30ef8b87ff2fd5b55063e491c058a639cd3f5
Petronellasacco <petronellatebatso705@gmail.com>
2023-07-15 03:56:57 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,17 @@
 int main(void)
 {
 	int n = 97;
-	int n = 65;
+	int m = 65;
 
 	while (n <= 122)
 	{
 		putchar(n);
+		n++;
 	}
-	while (n <= 90)
+	while (m <= 90)
 	{
 		putchar(n);
+		n++;
 	}
 	putchar('\n');
 	return (0);
